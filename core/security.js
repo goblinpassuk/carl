9b2980271dee0916cc0fb45c7189bef4db818e87
goblinpassuk <unused-ion-diary@duck.com>
2026-06-11 16:14:49 +0100

    dd

--- core/security.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const GOBLINPASS_CORE_FILE_WARNING = "If core/password-generator.js or core/security.js changes, review the fork before trusting generated passwords.";
-window.GOBLINPASS_ENGINE_VERSION = "2.0.0";
